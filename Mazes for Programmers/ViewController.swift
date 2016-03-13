a0a56835a74307a4c873062107af9d9ec3efc5f7
Thomas Elders <tomelders@thomass-imac.local>
2016-03-13 15:25:13 +0000

    Initial Commit

---
+++ Mazes for Programmers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mazes for Programmers
+//
+//  Created by Thomas Elders on 13/03/2016.
+//  Copyright © 2016 Thomas Elders. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
